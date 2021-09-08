9baeadb3ea9742f90bd2959717cee1b92658dc14
Maciej <macko939@gmail.com>
2021-09-08 19:34:12 +0100

    Update ISS_pass.py

--- ISS_pass.py
+++ ISS_pass.py
@@ -1,15 +1,22 @@
 # This program checks with the API to estimate when is the next time ISS will fly over London
 #
-# Import liobrary used to sent HTTP requests
+# Import library used to sent HTTP requests
 import requests
-
-# Pass save required parameters (Latitude and longitude) into a variable, 51N 0.3E is the approximate location of London 
+import datetime
+# Pass save required parameters (Latitude and longitude) into a variable, 51N 0.3E is the approximate location of London
 query = {'lat':'51', 'lon':'0.3'}
 # Send an API call to the REST API, passing my query variable as a 2nd argument to filter the results
 response = requests.get('http://api.open-notify.org/iss-pass.json', params=query)
+# Print the HTTP status code (200 = OK)
+print(response)
+# Save API response values to a new variable
+response_values = response.json()
 
-# Print the contents of the returned json file
-print(response.json())
+# Extract the unix time from the json
+datetime_request = int(response_values.get('request').get('datetime'))
 
+# Convert unix time to UTC format
+datetime_handle = datetime.datetime.fromtimestamp(int(datetime_request)).strftime('%Y-%m-%d %H:%M:%S')
 
-
+# Print the output
+print('ISS will pass over london on:\n' + datetime_handle + ' UTC')
